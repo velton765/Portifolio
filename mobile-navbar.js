0475b7571874a7cd16a8cd2c477c2018f5090e21
Velton Lopes de Almeida <59628178+velton765@users.noreply.github.com>
2021-08-20 20:02:46 -0300

    Update mobile-navbar.js

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,4 +1,3 @@
-  
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
       this.mobileMenu = document.querySelector(mobileMenu);
@@ -44,4 +43,45 @@ class MobileNavbar {
   );
   mobileNavbar.init();
 
+  window.addEventListener("DOMContentLoaded", function () {
+ 
+  
+    var form = document.getElementById("my-form");
+  
+    var status = document.getElementById("status");
+  
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "obrigado!";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! ocorreu algum problema.";
+    }
+  
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
   
